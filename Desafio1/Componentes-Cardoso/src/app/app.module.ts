bde1102e15479d640680b6936e4bad76d5f75e34
jamilecadias <jamilecadias@gmail.com>
2023-01-17 22:44:00 -0300

    Agregando cambios

--- Desafio1/Componentes-Cardoso/src/app/app.module.ts
+++ Desafio1/Componentes-Cardoso/src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ToolbarComponent } from './components/toolbar/toolbar.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { ContentComponent } from './components/content/content.component';
+import { InfoComponent } from './components/info/info.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ToolbarComponent,
+    NavbarComponent,
+    ContentComponent,
+    InfoComponent
   ],
   imports: [
     BrowserModule
