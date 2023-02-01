a7cfbfc515286e7e8af39deeb0e28d5693f3e6aa
jamilecadias <jamilecadias@gmail.com>
2023-02-01 15:38:34 -0300

    Agregando navbar y footer

--- desafio2/lista-cardoso/src/app/app.module.ts
+++ desafio2/lista-cardoso/src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ListaAlumnosComponent } from './components/lista-alumnos/lista-alumnos.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListaAlumnosComponent,
+    NavbarComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
