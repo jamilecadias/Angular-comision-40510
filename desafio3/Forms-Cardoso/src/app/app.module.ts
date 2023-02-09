74e8a9465420aa4dd9a2954dfd4d0132bcde500d
jamilecadias <jamilecadias@gmail.com>
2023-02-09 12:03:51 -0300

    Agregando componentes

--- desafio3/Forms-Cardoso/src/app/app.module.ts
+++ desafio3/Forms-Cardoso/src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ToobarComponent } from './components/toobar/toobar.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { FormComponent } from './components/form/form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ToobarComponent,
+    NavbarComponent,
+    FooterComponent,
+    FormComponent
   ],
   imports: [
     BrowserModule
