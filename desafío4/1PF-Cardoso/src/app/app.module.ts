91f0a54199aba3d9e15d3d9b08dcdc1c72b1b9af
jamilecadias <jamilecadias@gmail.com>
2023-02-14 12:10:29 -0300

    Agregando componentes

--- desafío4/1PF-Cardoso/src/app/app.module.ts
+++ desafío4/1PF-Cardoso/src/app/app.module.ts
@@ -6,6 +6,8 @@ import { ToolbarComponent } from './components/toolbar/toolbar.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { ListaComponent } from './components/lista/lista.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { EditarCursoDialogComponent } from './components/editar-curso-dialog/editar-curso-dialog.component';
 
 @NgModule({
   declarations: [
@@ -13,12 +15,51 @@ import { ListaComponent } from './components/lista/lista.component';
     ToolbarComponent,
     NavbarComponent,
     FooterComponent,
-    ListaComponent
+    ListaComponent,
+    EditarCursoDialogComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+
+
+/* import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialComponent } from './components/material/material.component';
+import { MaterialModule } from './material.module';
+import { EditarCursoDialogComponent } from './components/editar-curso-dialog/editar-curso-dialog.component';
+import { FormatoFechaPipe } from './pipes/formato-fecha.pipe';
+import { BooleanoATextoPipe } from './pipes/booleano-a-texto.pipe';
+import { BooleanoEstiloDirective } from './directives/booleano-estilo.directive';
+import { FiltroCursosPipe } from './pipes/filtro-cursos.pipe';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MaterialComponent,
+    EditarCursoDialogComponent,
+    FormatoFechaPipe,
+    BooleanoATextoPipe,
+    BooleanoEstiloDirective,
+    FiltroCursosPipe
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    MaterialModule,
+    ReactiveFormsModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { } */
---
+++ desafío4/1PF-Cardoso/src/app/components/editar-curso-dialog/editar-curso-dialog.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-editar-curso-dialog',
+  templateUrl: './editar-curso-dialog.component.html',
+  styleUrls: ['./editar-curso-dialog.component.css']
+})
+export class EditarCursoDialogComponent {
+
+}
