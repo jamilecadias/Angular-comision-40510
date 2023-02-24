d15e2c05ae0a527ad4a8866a2e5771faad15ae58
jamilecadias <jamilecadias@gmail.com>
2023-02-24 18:23:41 -0300

    Clase 9

---
+++ 9. Servicios y RxJS/demo/src/app/app.module.ts
@@ -0,0 +1,45 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { ListaCursosComponent } from './components/lista-cursos/lista-cursos.component';
+import { CursoService } from './services/curso.service';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './material.module';
+import { TablaCursosComponent } from './components/tabla-cursos/tabla-cursos.component';
+import { CursoAlphaService } from './services/curso-alpha.service';
+import { cursos } from './services/cursos.data';
+import { env } from 'src/environment/environment';
+import { config, token } from './config';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ListaCursosComponent,
+    TablaCursosComponent
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    MaterialModule
+  ],
+  providers: [
+    // CursoService,
+    // {provide: CursoService, useExisting: CursoAlphaService}
+    // {provide: CursoService, useClass: CursoAlphaService}
+    // {provide: CursoService, useValue: cursos}
+    {provide: CursoService, useFactory: ()=>{
+      if(env.utilizarServicioAlpha == 'Alpha'){
+        return new CursoAlphaService()
+      }else if(env.utilizarServicioAlpha == 'Legacy'){
+        return cursos
+      }else{
+        return new CursoService()
+      }
+    }
+    },
+    { provide: token, useValue: config }
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
